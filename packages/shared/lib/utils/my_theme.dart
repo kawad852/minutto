ee99c4529df67cbb54470266a85e77ee79f91770
khaledawad <kawad852@gmail.com>
2025-08-26 16:41:33 +0300

    merge

--- packages/shared/lib/utils/my_theme.dart
+++ packages/shared/lib/utils/my_theme.dart
@@ -41,7 +41,7 @@ class MyTheme {
           fontWeight: FontWeight.bold,
         ),
       ),
-      bottomAppBarTheme: BottomAppBarThemeData(color: Colors.white),
+      bottomAppBarTheme: BottomAppBarTheme(color: Colors.white),
       bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: Colors.white),
       scaffoldBackgroundColor: Colors.white,
       actionIconTheme: ActionIconThemeData(
