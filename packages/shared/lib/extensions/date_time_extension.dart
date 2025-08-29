6efa2714962446c6f181a7a621ddcc16c13b80f5
khaledawad <kawad852@gmail.com>
2025-08-29 08:28:21 +0300

    hour format

--- packages/shared/lib/extensions/date_time_extension.dart
+++ packages/shared/lib/extensions/date_time_extension.dart
@@ -3,9 +3,8 @@ import 'package:shared/exports/algolia_exports.dart' hide Filter;
 import '../shared.dart';
 
 extension DateTimeExtension on DateTime {
-  // String get defaultFormat => DateFormat().format(this);
-  String get yMMMMd => DateFormat.yMMMd('en_US').format(this);
   String get defaultFormat => DateFormat('19.02.2023').format(this);
+  String get yMMMMd => DateFormat.yMMMd('en_US').format(this);
 
   String get hourFormat => DateFormat.jm(MySharedPreferences.language).format(this);
 
