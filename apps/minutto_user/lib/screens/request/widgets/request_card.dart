3b6d13613d470e6d481f4e4c682073688fb9a109
khaledawad <kawad852@gmail.com>
2025-08-31 03:54:25 +0300

    requests

--- apps/minutto_user/lib/screens/request/widgets/request_card.dart
+++ apps/minutto_user/lib/screens/request/widgets/request_card.dart
@@ -11,80 +11,83 @@ class RequestCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        border: Border.all(color: context.colorPalette.greyEAE),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        spacing: 13,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              const CustomSvg(MyIcons.note),
-              const SizedBox(width: 7),
-              Expanded(
-                child: Text(
-                  "اجازة مرضية",
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: context.colorPalette.blue091,
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  context.navigate((context) => const RequestDetailsScreen());
-                },
-                child: Icon(Icons.arrow_forward_ios_rounded),
-              ),
-            ],
-          ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-            decoration: BoxDecoration(
-              color: context.colorPalette.greyF9F,
-              border: Border.all(color: context.colorPalette.greyEAE),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Row(
+    return GestureDetector(
+      onTap: () {
+        context.navigate((context) => const RequestDetailsScreen());
+      },
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(color: context.colorPalette.greyEAE),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          spacing: 13,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
               children: [
+                const CustomSvg(MyIcons.note),
+                const SizedBox(width: 7),
                 Expanded(
-                  flex: 2,
-                  child: RequestInfo(
-                    title: context.appLocalization.vacationDate,
-                    value: "25 -27 أغسطس",
+                  child: Text(
+                    LeaveReason.label(context, request.reason!),
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      color: context.colorPalette.blue091,
+                      fontSize: 15,
+                    ),
                   ),
                 ),
-                Expanded(
-                  child: RequestInfo(
-                    title: context.appLocalization.totalDays,
-                    value: "يومين",
+                Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  size: 17,
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              decoration: BoxDecoration(
+                color: context.colorPalette.greyF9F,
+                border: Border.all(color: context.colorPalette.greyEAE),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: RequestInfo(
+                      title: context.appLocalization.vacationDate,
+                      value: "25 -27 أغسطس",
+                    ),
+                  ),
+                  Expanded(
+                    child: RequestInfo(
+                      title: context.appLocalization.totalDays,
+                      value: "يومين",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              spacing: 10,
+              children: [
+                const CustomSvg(MyIcons.checkIcon),
+                Text(
+                  "جاري انتظار الرد",
+                  style: TextStyle(
+                    color: context.colorPalette.yellowF69,
+                    fontSize: 14,
                   ),
                 ),
               ],
             ),
-          ),
-          Row(
-            spacing: 10,
-            children: [
-              const CustomSvg(MyIcons.checkIcon),
-              Text(
-                "جاري انتظار الرد",
-                style: TextStyle(
-                  color: context.colorPalette.yellowF69,
-                  fontSize: 14,
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
