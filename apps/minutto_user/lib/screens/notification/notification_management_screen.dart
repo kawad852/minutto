cdde4d6876a8eba7b88750faa6aac4d3fc4d5e97
khaledawad <kawad852@gmail.com>
2025-09-10 07:58:59 +0300

    notifications

--- apps/minutto_user/lib/screens/notification/notification_management_screen.dart
+++ apps/minutto_user/lib/screens/notification/notification_management_screen.dart
@@ -9,10 +9,33 @@ class NotificationManagementScreen extends StatefulWidget {
 
 class _NotificationManagementScreenState extends State<NotificationManagementScreen> {
   final _formKey = GlobalKey<FormState>();
+  String? _selectedBranchId;
+  final _notification = NotificationModel(
+    data: NotificationDataModel(
+      type: "",
+    ),
+    notification: NotificationHeaderModel(),
+  );
 
   void _onSubmit(BuildContext context) {
     if (_formKey.currentState!.validate()) {
       context.unFocusKeyboard();
+      ApiService.fetch(
+        context,
+        callBack: () async {
+          final functions = FirebaseFunctions.instanceFor(region: "europe-west3");
+          await functions.httpsCallable('broadcastGeneralNotification').call({
+            "companyId": MySharedPreferences.company!.id,
+            if (_selectedBranchId != null) "branchId": _selectedBranchId,
+            "title": _notification.notification!.title,
+            "body": _notification.notification!.body,
+          });
+          if (context.mounted) {
+            context.showSnackBar(context.appLocalization.sentSuccessfully);
+            Navigator.pop(context);
+          }
+        },
+      );
     }
   }
 
@@ -23,7 +46,7 @@ class _NotificationManagementScreenState extends State<NotificationManagementScr
         title: Text(context.appLocalization.sendNewNotification),
       ),
       bottomNavigationBar: BottomButton(
-        onPressed: () {},
+        onPressed: () => _onSubmit(context),
         backgroundColor: context.colorPalette.blue091,
         textColor: context.colorPalette.white,
         title: context.appLocalization.send,
@@ -36,14 +59,16 @@ class _NotificationManagementScreenState extends State<NotificationManagementScr
             children: [
               WidgetTitle(
                 title: context.appLocalization.notificationTitle,
-                child: CustomTextField.text(onChanged: (value) {}),
+                child: CustomTextField.text(
+                  onChanged: (value) => _notification.notification!.title = value!,
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 15),
                 child: WidgetTitle(
                   title: context.appLocalization.notificationText,
                   child: CustomTextField.text(
-                    onChanged: (value) {},
+                    onChanged: (value) => _notification.notification!.body = value!,
                     maxLines: 5,
                   ),
                 ),
