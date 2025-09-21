1eebc7f5425985048d87989e15c488c998532510
khaledawad <kawad852@gmail.com>
2025-09-21 15:10:39 +0300

    upgrades

--- packages/shared/lib/shared_pods.rb
+++ packages/shared/lib/shared_pods.rb
@@ -1,5 +1,5 @@
 # shared_pods.rb
 
 def add_shared_pods
-  pod 'FirebaseFirestore', :git => 'https://github.com/invertase/firestore-ios-sdk-frameworks.git', :tag => '12.0.0'
+  pod 'FirebaseFirestore', :git => 'https://github.com/invertase/firestore-ios-sdk-frameworks.git', :tag => '12.2.0'
 end
